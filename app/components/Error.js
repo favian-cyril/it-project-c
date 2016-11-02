b50d692e02bde7c6ba420ccf0937a52a4cfd2629
chadmadna <chadmadna@gmail.com>
2016-11-03 04:03:58 +0700

    Changed search error styling

--- app/components/Error.js
+++ app/components/Error.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Error = props =>
   <div className="search-error dropdown-menu">
-    <h4 className="text-center">{props.msg}</h4>
-    <p className="text-center">{props.desc}</p>
+    <h4 className="text-xs-center">{props.msg}</h4>
+    <p className="text-xs-center text-muted">{props.desc}</p>
   </div>
 
 Error.propTypes = {
