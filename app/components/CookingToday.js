977ea532e39ae753efe42ec06a8b65d74f8e9fb8
metandrini <meanut.peanut@gmail.com>
2016-11-08 15:34:17 +0700

    Committed semi-working Cooking Today

--- app/components/CookingToday.js
+++ app/components/CookingToday.js
@@ -1,32 +1,58 @@
 import React from 'react'
 
-const CookingToday = (props) => {
-  // Fetch data from props
+const CookingToday = (props, context) => {
+  function missingList(props) {
+    return (
+      <li className="list-group-item">{props.name}</li>
+    )
+  }
+  function panel(props) {
+    const missing = props.recipe.missedIngredients.map(item => item.name)
+    return (
+      <div className="panel">
+        <ul className="list-group">
+          {missing.map((item) => <missingList key={item} name={item}/>)}
+        </ul>
+      </div>
+    )
+  }
+  const caretClass = !context.isExpanded ? 'fa-caret-right' : 'fa-caret-down'
+
   return (
-    // JSX/HTML markup for the component
-    // You will use props here
-  <button className="accordion">
-    <span className="fa fa-caret-right"></span>
-    <span className="fa fa-caret-down" style="display: none;"></span>
-    {props.recipe.name}
-    <a
-      className="fa fa-2x fa-external-link btn-add-icon"
-      href="{props.recipe.sourceUrl}"
-    >
-    </a>
-  </button>
-  <div className="panel">
-    <ul className="list-group">
-    <li className="list-group-item">
-    // missing ingredients
-    </li>
-    </ul>
+    <div className="list-wrapper">
+      <button className="accordion" onClick={context.toggleAccordion}>
+        <span className={`fa ${caretClass}`}></span>
+        {props.recipe.title}
+        <a
+          className="btn btn-default btn-add"
+          href={props.recipe.sourceUrl}
+          target="_blank" rel="noopener noreferrer"
+        >
+          <i className="fa fa-2x fa-external-link"/>
+        </a>
+      </button>
+      {
+        context.isExpanded ? <panel/> : null
+      }
     </div>
   )
 }
 
 CookingToday.propTypes = {
-  // props you expect from CookingTodayList
+  recipe: React.PropTypes.shape({
+    title: React.PropTypes.string.isRequired,
+    sourceUrl: React.PropTypes.string.isRequired,
+    missedIngredients: React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        name: React.PropTypes.string.isRequired
+      }).isRequired
+    ).isRequired
+  }).isRequired
+}
+
+CookingToday.contextTypes = {
+  isExpanded: React.PropTypes.bool,
+  toggleAccordion: React.PropTypes.func
 }
 
 export default CookingToday
--- app/components/CookingTodayList.js
+++ app/components/CookingTodayList.js
@@ -1,39 +1,30 @@
 import React from 'react'
 import CookingToday from './CookingToday'
 
-const CookingTodayList = (props) => {
-  // Fetch data from props, which would be Dashboard
+const CookingTodayList = (props, context) => {
+  // Fetch data from props and context, which would be Dashboard
   // Map all props.cookingToday() to create multiple CookingToday
   let results = context.recipes.map((item, i) =>
-    <CookingToday key={i} recipe={item}/>
+      <CookingToday key={i} recipe={item}/>
   )
-  if (props.cookingToday.length > 0) {
-    results = props.cookingToday.map((recipe) => {
-      return <CookingToday someProp={recipe.prop} />
-    })
+  if (context.recipes.length === 0) {
+    results = (
+      <div className="list-wrapper">
+      </div>
+    )
   }
   return (
-    // JSX/HTML markup for the component
-    // You will use props here
     <div className="card">
       <div className="card-block">
         <h5 className="card-title">Cooking Today</h5>
       </div>
-      <div className="list-wrapper">
-        {results}
-      </div>
+      {results}
       <div className="card-footer">
-        <center>
-          <button type="button" className="btn btn-success">Add to Shopping List</button>
-          <button type="button" className="btn btn-danger">Clear All Recipes</button>
-        </center>
+        <button type="button" className="btn btn-success">Add to Shopping List</button>
+        <button type="button" className="btn btn-danger">Clear All Recipes</button>
       </div>
     </div>
   )
-}
-
-CookingTodayList.propTypes = {
-  // props you expect from CookingTodayList
 }
 
 CookingTodayList.contextTypes = {
