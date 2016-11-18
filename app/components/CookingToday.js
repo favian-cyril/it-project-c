ebd563aea53c4fd34dadaa2169a83c131d5f4d97
metandrini <meanut.peanut@gmail.com>
2016-11-19 01:53:40 +0700

    Minor accordion fixes

--- app/components/CookingToday.js
+++ app/components/CookingToday.js
@@ -2,21 +2,19 @@ import React from 'react'
 import CookingTodayIngredient from './CookingTodayIngredient'
 
 const CookingToday = (props) => {
-  const accordionClass = props.isExpanded.expand ? 'active' : ''
-  const caretClass = props.isExpanded.expand ? 'fa-caret-down' : 'fa-caret-right'
+  const accordionClass = props.isExpanded.expand && props.id === props.isExpanded.id ? 'active' : ''
+  const caretClass = props.isExpanded.expand && props.id === props.isExpanded.id ? 'fa-caret-down' : 'fa-caret-right'
   const panelClass = props.isExpanded.expand && props.id === props.isExpanded.id ? 'show' : ''
 
   return (
     <div>
       <button className={`accordion ${accordionClass}`} onClick={() => {props.toggleAccordion(props.id)}} id={props.id}>
         <span className={`fa ${caretClass}`}></span>
-        {props.recipe.title}
         <a
-          className="btn btn-sm btn-default btn-add"
           href={props.recipe.sourceUrl}
           target="_blank" rel="noopener noreferrer"
         >
-          <i className="fa fa-2x fa-external-link"/>
+          {props.recipe.title}
         </a>
       </button>
         <div className={`panel ${panelClass}`} id={props.id}>
